0136406b3d706671e98b587ecf79073516dde62f
enyes <nirmalyasahoo19@gmail.com>
2026-05-15 21:19:12 +0530

    Refactor birthday animation and music functions

--- bday.js
+++ bday.js
@@ -1,37 +1,51 @@
-// Stars
+// Stars Animation
+
 const starsContainer = document.querySelector('.stars');
 
 for(let i=0;i<150;i++){
-  const star = document.createElement('div');
-  star.classList.add('star');
 
-  star.style.left = Math.random()*100 + '%';
-  star.style.top = Math.random()*100 + '%';
+    const star = document.createElement('div');
 
-  star.style.animationDuration = (Math.random()*3+1) + 's';
+    star.classList.add('star');
 
-  starsContainer.appendChild(star);
+    star.style.left = Math.random()*100 + '%';
+    star.style.top = Math.random()*100 + '%';
+
+    star.style.animationDuration =
+    (Math.random()*3+1) + 's';
+
+    starsContainer.appendChild(star);
 }
 
-// Confetti
+
+// Confetti Animation
+
 for(let i=0;i<120;i++){
-  const confetti = document.createElement('div');
-  confetti.classList.add('confetti');
 
-  confetti.style.left = Math.random()*100 + 'vw';
-  confetti.style.backgroundColor =
-  `hsl(${Math.random()*360},100%,50%)`;
+    const confetti = document.createElement('div');
 
-  confetti.style.animationDuration =
-  (Math.random()*3+2) + 's';
+    confetti.classList.add('confetti');
 
-  confetti.style.opacity = Math.random();
+    confetti.style.left =
+    Math.random()*100 + 'vw';
 
-  document.body.appendChild(confetti);
+    confetti.style.backgroundColor =
+    `hsl(${Math.random()*360},100%,50%)`;
+
+    confetti.style.animationDuration =
+    (Math.random()*3+2) + 's';
+
+    confetti.style.opacity = Math.random();
+
+    document.body.appendChild(confetti);
 }
+
+
+// Birthday Music Button
 
 function playWish(){
-  document.getElementById("song").play();
 
-  alert("🎂 Happiest Birthday Smruti 💖✨");
+    document.getElementById("song").play();
+
+    alert("🎂 Happiest Birthday Smruti 💖✨");
 }
