d9ee703f75e5bab9546200ba0d12e672ba85f267
enyes <nirmalyasahoo19@gmail.com>
2026-05-15 21:05:26 +0530

    Add birthday celebration animations and song
    This script generates stars and confetti animations for a birthday celebration and includes a function to play a birthday song.

---
+++ bday.js
@@ -0,0 +1,37 @@
+// Stars
+const starsContainer = document.querySelector('.stars');
+
+for(let i=0;i<150;i++){
+  const star = document.createElement('div');
+  star.classList.add('star');
+
+  star.style.left = Math.random()*100 + '%';
+  star.style.top = Math.random()*100 + '%';
+
+  star.style.animationDuration = (Math.random()*3+1) + 's';
+
+  starsContainer.appendChild(star);
+}
+
+// Confetti
+for(let i=0;i<120;i++){
+  const confetti = document.createElement('div');
+  confetti.classList.add('confetti');
+
+  confetti.style.left = Math.random()*100 + 'vw';
+  confetti.style.backgroundColor =
+  `hsl(${Math.random()*360},100%,50%)`;
+
+  confetti.style.animationDuration =
+  (Math.random()*3+2) + 's';
+
+  confetti.style.opacity = Math.random();
+
+  document.body.appendChild(confetti);
+}
+
+function playWish(){
+  document.getElementById("song").play();
+
+  alert("🎂 Happiest Birthday Smruti 💖✨");
+}
